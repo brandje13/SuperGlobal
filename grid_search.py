083a190a09667a440355e8387743d192c3c2d170
Nick <39793720+brandje13@users.noreply.github.com>
2026-07-18 17:10:42 +0200

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -9,6 +9,7 @@ import gc
 import pickle
 from tqdm import tqdm
 import numpy as np
+import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import config as config
@@ -463,4 +464,8 @@ def main():
 
 
 if __name__ == "__main__":
+    # --- LINUX FORK DEADLOCK FIX ---
+    # Force the multiprocessing module to use 'spawn' to prevent worker processes
+    # from inheriting locked PyTorch mutexes and freezing indefinitely.
+    mp.set_start_method('spawn', force=True)
     main()
